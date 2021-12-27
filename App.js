1edc6aa0ce77ce8f10499db3e9b6fa3e8c36ac2e
Marcelo Campanelli <marcelo.campanelli@yahoo.com>
2021-12-26 22:15:40 -0300

    tela de login simples com animacao

--- App.js
+++ App.js
@@ -1,15 +1,161 @@
-import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import {
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  Image,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Animated,
+  Keyboard,
+} from "react-native";
 
 export default function App() {
+  const [offSet] = useState(new Animated.ValueXY({ x: 0, y: 96 }));
+  const [opacity] = useState(new Animated.Value(0));
+  const [logo] = useState(new Animated.ValueXY({ x: 130, y: 155 }));
+
+  useEffect(() => {
+    keyboardDidShowListener = Keyboard.addListener(
+      "keyboardDidShow",
+      keyboardDidShow
+    );
+    keyboardDidHideShow = Keyboard.addListener(
+      "keyboardDidHide",
+      keyboardDidHide
+    );
+
+    Animated.parallel([
+      Animated.spring(offSet.y, {
+        toValue: 0,
+        speed: 4,
+        bounciness: 30,
+        useNativeDriver: true,
+      }),
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 200,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, []);
+
+  function keyboardDidShow() {
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 55,
+        duration: 100,
+      }),
+      Animated.timing(logo.y, {
+        toValue: 75,
+        duration: 100,
+      }),
+    ]).start();
+  }
+
+  function keyboardDidHide() {
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 130,
+        duration: 100,
+      }),
+      Animated.timing(logo.y, {
+        toValue: 155,
+        duration: 100,
+      }),
+    ]).start();
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.containerLogo}>
+        <Animated.Image
+          style={{
+            width: logo.x,
+            height: logo.y,
+          }}
+          source={require("./assets/img/logo.png")}
+        />
+      </View>
+
+      <Animated.View
+        style={[
+          styles.container,
+          {
+            opacity: opacity,
+            transform: [{ translateY: offSet.y }],
+          },
+        ]}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
+
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.submitText}>Acessar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btnRegister}>
+          <Text style={styles.registerText}>Criar conta</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#191919",
+  },
+  containerLogo: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  container: {
+    paddingBottom: 30,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "90%",
+  },
+  input: {
+    backgroundColor: "#FFF",
+    width: "90%",
+    marginBottom: 15,
+    color: "#222",
+    fontSize: 17,
+    borderRadius: 7,
+    padding: 10,
+  },
+  btnSubmit: {
+    backgroundColor: "#35aaff",
+    width: "90%",
+    height: 45,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 7,
+  },
+  submitText: {
+    color: "#FFF",
+    fontSize: 18,
+  },
+  btnRegister: {
+    marginTop: 10,
+  },
+  registerText: {
+    color: "#FFF",
+  },
+});
